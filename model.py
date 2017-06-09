8d03bb9acaa2b5211ee2249ff338869553bb1fa0
Malcolm <malichaooo@gmail.com>
2017-06-08 19:50:08 -0700

    Add normalization layer

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 csv_data = np.genfromtxt("data/driving_log.csv", delimiter=',',\
                          names=True, dtype=None)
@@ -24,10 +24,11 @@ X_train =  np.array(images)
 y_train =  np.array(measurements)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x /255.,input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=7)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=15)
 
 model.save("model.h5")
