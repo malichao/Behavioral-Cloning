24ada2a4a2352543ce7ca132feb1605f26c1bfef
Malcolm <malichaooo@gmail.com>
2017-06-08 20:02:45 -0700

    Fine tune parameters

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from keras.layers import Flatten, Dense, Lambda
 
 csv_data = np.genfromtxt("data/driving_log.csv", delimiter=',',\
                          names=True, dtype=None)
-FILE_NUM=1000
+FILE_NUM=2000
 measurements = [t[3] for t in csv_data]
 file_names =[t[0].decode('utf-8') for t in csv_data]
 print("Read ",len(file_names)," files, only use first",FILE_NUM," ones")
@@ -24,11 +24,11 @@ X_train =  np.array(images)
 y_train =  np.array(measurements)
 
 model = Sequential()
-model.add(Lambda(lambda x: x /255.,input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x /255. ,input_shape=(160,320,3)))
 model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=15)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=5)
 
 model.save("model.h5")
