b755610f903b0f97293b5ca840c92ece0cf543e4
Malcolm <malichaooo@gmail.com>
2017-06-10 13:12:17 -0700

    Use generator for trainning, result is not as good as ad-hoc generator

--- model.py
+++ model.py
@@ -1,51 +1,26 @@
-import csv
-import cv2
-import random
-import numpy as np
+import utils
 from keras.models import Sequential
 from keras.layers import Cropping2D
-from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda 
+from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
 
-csv_data = np.genfromtxt("data/driving_log.csv", delimiter=',',
-                         names=True, dtype=None)
-FILE_NUM = 800
-print("Read ", len(csv_data), " files, only use first", FILE_NUM, " ones")
-measurements_raw = [t[3] for t in csv_data]
-file_names = [[t[0].decode('utf-8'), t[1].decode('utf-8'),
-               t[2].decode('utf-8')] for t in csv_data]
-measurements_raw = measurements_raw[0:FILE_NUM]
-file_names = file_names[0:FILE_NUM]
+# My simple sample reading
+#X_train,y_train = utils.generate_train_data("data/")
 
-images = []
-measurements=[]
-source_path = "data/"
-selected = [i for i in range(0,len(file_names))]
-random.shuffle(selected)
-print("Shuffle inputs.. ",selected[0:10])
-for i in range(0,FILE_NUM):
-    files = file_names[selected[i]]
-    mea = measurements_raw[selected[i]]
-    for file in files:
-        if file[0]==' ':    # Stupid space...
-            file = file[1:]
-        file_name = source_path + file
-        image = cv2.imread(file_name)
-        image_flipped = np.fliplr(image)
-        images.append(image)
-        measurements.append(mea)
+path = 'data/'
+samples = utils.csv2samples(path + 'driving_log.csv')
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_generator = utils.generator(path,train_samples, batch_size=900)
+validation_generator = utils.generator(path,validation_samples, batch_size=900)
 
-#measurements = [-mea for mea in measurements]
-
-X_train = np.array(images)
-y_train = np.array(measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255. - .5, input_shape=(160, 320, 3)))
 # Crop 50 rows from the top, 20 rows from the bottom
 # Crop 0 columns from the left, 0 columns from the right
-model.add(Cropping2D(cropping=((60,23), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((60, 23), (0, 0)), input_shape=(160, 320, 3)))
 model.add(Convolution2D(6, 5, 5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation="relu"))
@@ -56,6 +31,9 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), \
+                    validation_data=validation_generator, \
+                    nb_val_samples=len(validation_samples), nb_epoch=6)
 
 model.save("model.h5")
--- utils.py
+++ utils.py
@@ -1,42 +1,64 @@
 import os
 import cv2
+import csv
 import sklearn
 import numpy as np
 from random import shuffle
 from sklearn.model_selection import train_test_split
 
-def generator(samples, batch_size=32):
+def csv2samples(file):
+    samples = []
+    with open(file) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+    return samples
+
+def generator(path, samples, batch_size=32):
     """
-    Generate a bath of training data
+    Generate a batch of training data
     """
     num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
+    while 1:  # Loop forever so the generator never terminates
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
+            batch_samples = samples[offset:offset + batch_size]
             images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+            measurements = []
+            for sample in batch_samples:
+                files = [sample[0], sample[1], sample[2]]
+                mea = float(sample[3])
+                # Read left, center and right images
+                # for file in files:
+                #     file_name = path + file
+                #     image = cv2.imread(file_name)
+                #     images.append(image)
+                #     measurements.append(mea)
+
+
+                file_name = path + files[0]
+                image = cv2.imread(file_name)
+                images.append(image)
+                measurements.append(mea)
+
 
             # trim image to only see section with road
             X_train = np.array(images)
-            y_train = np.array(angles)
+            y_train = np.array(measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
 
+
 def generate_train_data(path):
-    csv_data = np.genfromtxt(path+"driving_log.csv", delimiter=',',
-                         names=True, dtype=None)
+    """
+    Generate a fix batch of random training data from a path
+    """
+    csv_data = np.genfromtxt(path + "driving_log.csv", delimiter=',',
+                             names=True, dtype=None)
     FILE_NUM = 1200
     print("Read ", len(csv_data), " files, only use first", FILE_NUM, " ones")
     measurements_raw = [t[3] for t in csv_data]
     file_names = [[t[0].decode('utf-8'), t[1].decode('utf-8'),
-                t[2].decode('utf-8')] for t in csv_data]
+                   t[2].decode('utf-8')] for t in csv_data]
     measurements_raw = measurements_raw[0:FILE_NUM]
     file_names = file_names[0:FILE_NUM]
 
@@ -60,6 +82,6 @@ def generate_train_data(path):
 
     X_train = np.array(images)
     y_train = np.array(measurements)
-    print("X_train = ",X_train.shape)
-    print("y_train = ",y_train.shape)
-    return X_train,y_train
+    print("X_train = ", X_train.shape)
+    print("y_train = ", y_train.shape)
+    return X_train, y_train
